6dbdef04149ca77906e71ad0abb96f6989564bbf
Felipe Sobral <xfelipesobral@gmail.com>
2021-03-27 03:16:28 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Routes from './routes'
+import './core/css/principal.css'
+
+import Cabecalho from './core/components/Cabecalho'
+
+
+function App() {
+
+	return (
+		<div>
+			<Cabecalho />
+			<Routes />
+		</div>
+	)
+}
+
+export default App
---
+++ src/core/components/Cabecalho.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Cabecalho() {
+
+    return (
+        <div>
+            <a>cabecalhoaaaaaaaa</a>
+        </div>
+    )
+}
+
+export default Cabecalho
